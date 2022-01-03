7012a1372ff5efae43c6af1e54a6144447b501a3
Sama Samrin Orpa <samasamrin@gmail.com>
2022-01-03 22:09:07 +0600

    Update maxSubArray.py
    Added runtime, memory and variable definitions as comments

--- maxSubArray.py
+++ maxSubArray.py
@@ -8,14 +8,13 @@ from sys import maxint
 
 def maxSubArray(self, nums):
         size = len(nums)
-        max_so_far = -maxint - 1
-        max_ending_here = 0
+        max_so_far = -maxint - 1 #maximum subarray ending at this index
+        max_ending_here = 0  #maximum subarray overall so far
 
         for i in range(0, size):
             max_ending_here = max_ending_here + nums[i]
             if (max_so_far < max_ending_here):
                 max_so_far = max_ending_here
-
             if max_ending_here < 0:
                 max_ending_here = 0   
         return max_so_far
@@ -30,3 +29,7 @@ c= maxSubArray([1]) #answer 1
 print("3rd Answer: ", c)
 
 
+"""
+  Runtime = 596 ms
+  Memory = 25.7 MB
+"""
