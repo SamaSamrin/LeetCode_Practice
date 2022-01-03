8c14a2e7c91c1973683dc0eb1c1b2895e5bb1e6c
Sama Samrin Orpa <samasamrin@gmail.com>
2022-01-03 22:01:21 +0600

    Add files via upload
    A solution to the LeetCode problem #53 based on Kadane's algorithm of finding the maximum contiguous subarray.
    
    Kadane's algorithm is explained here: https://www.youtube.com/watch?v=86CQq3pKSUw

---
+++ maxSubArray.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 20:58:34 2022
+
+@author: ASUS
+"""
+from sys import maxint
+
+def maxSubArray(self, nums):
+        size = len(nums)
+        max_so_far = -maxint - 1
+        max_ending_here = 0
+
+        for i in range(0, size):
+            max_ending_here = max_ending_here + nums[i]
+            if (max_so_far < max_ending_here):
+                max_so_far = max_ending_here
+
+            if max_ending_here < 0:
+                max_ending_here = 0   
+        return max_so_far
+
+a = maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) #answer 6
+print("1st Answer: ", a)
+
+b= maxSubArray([5,4,-1,7,8]) #answer 23
+print("2nd Answer: ", b)
+
+c= maxSubArray([1]) #answer 1
+print("3rd Answer: ", c)
+
+
