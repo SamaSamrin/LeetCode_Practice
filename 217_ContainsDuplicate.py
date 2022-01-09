0c2ac2f11f9382eb2a2c3d1d35d2d06106889320
Sama Samrin Orpa <samasamrin@gmail.com>
2022-01-09 21:21:42 +0600

    Update 217_ContainsDuplicate.py

--- 217_ContainsDuplicate.py
+++ 217_ContainsDuplicate.py
@@ -8,8 +8,8 @@ Created on Mon Jan  3 18:26:33 2022
 def containsDuplicate(nums):
         print(set(nums))
         if len(nums)==len(set(nums)):
-            return True
-        else:
             return False
+        else:
+            return True
         
 print(containsDuplicate([1,2,3,1]))
